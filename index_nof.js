dedc32d85dffd8b9ad07acea37e74f2874a73836
leandroros <leandro.oliveira@epsoft.com.br>
2021-12-16 16:24:41 -0300

    novapage

--- index_nof.js
+++ index_nof.js
@@ -13,18 +13,26 @@ $(document).ready(function(){
 // Begin Navigation Bars
 var ButtonsImageMapping = [];
 ButtonsImageMapping["NavigationBar1"] = {
-	"Botãodenavegação1" : { image: "./Home_Hp_Highlighted_1.jpg", rollover: "./Home_HRp_HighlightedOver_1.jpg", w: 101, h: 36 },
-	"Botãodenavegação2" : { image: "./Login_Np_Regular_1.jpg", rollover: "./Login_NRp_RegularOver_1.jpg", w: 101, h: 36, opening: "bottom", offsetX: 0, offsetY: 36 },
-	"Botãodenavegação4" : { image: "./Adm_Ns_Regular.png", rollover: "./Adm_NRs_RegularOver.png", w: 101, h: 21 },
-	"Botãodenavegação5" : { image: "./Senha_Ns_Regular.png", rollover: "./Senha_NRs_RegularOver.png", w: 101, h: 21 },
-	"Botãodenavegação6" : { image: "./cadastro_Ns_Regular.png", rollover: "./cadastro_NRs_RegularOver.png", w: 101, h: 21 },
-	"Botãodenavegação3" : { image: "./Tabiban_Np_Regular_1.jpg", rollover: "./Tabiban_NRp_RegularOver_1.jpg", w: 101, h: 36 }
+	"Botãodenavegação1" : { image: "./Home_Hp_highlighted.png", rollover: "./Home_HRp_highlightedOver.png", w: 120, h: 57 },
+	"Botãodenavegação2" : { image: "./Sobre_Np_regular.png", rollover: "./Sobre_NRp_regularOver.png", w: 120, h: 57, opening: "bottom", offsetX: 0, offsetY: 57 },
+	"Botãodenavegação6" : { image: "./Depoimentos_Ns_regular.png", rollover: "./Depoimentos_NRs_regularOver.png", w: 120, h: 46 },
+	"Botãodenavegação7" : { image: "./Servi-os_Ns_regular.png", rollover: "./Servi-os_NRs_regularOver.png", w: 120, h: 46 },
+	"Botãodenavegação3" : { image: "./Fale-conosco_Np_regular.png", rollover: "./Fale-conosco_NRp_regularOver.png", w: 120, h: 57, opening: "bottom", offsetX: 0, offsetY: 57 },
+	"Botãodenavegação8" : { image: "./Localiza--o_Ns_regular.png", rollover: "./Localiza--o_NRs_regularOver.png", w: 120, h: 46 },
+	"Botãodenavegação4" : { image: "./Eventos_Np_regular.png", rollover: "./Eventos_NRp_regularOver.png", w: 120, h: 57, opening: "bottom", offsetX: 0, offsetY: 57 },
+	"Botãodenavegação9" : { image: "./Calend-rio_Ns_regular.png", rollover: "./Calend-rio_NRs_regularOver.png", w: 120, h: 46 },
+	"Botãodenavegação10" : { image: "./Galeria-de-Fotos_Ns_regular.png", rollover: "./Galeria-de-Fotos_NRs_regularOver.png", w: 120, h: 46 },
+	"Botãodenavegação11" : { image: "./Fotos_Ns_regular.png", rollover: "./Fotos_NRs_regularOver.png", w: 120, h: 46 },
+	"Botãodenavegação5" : { image: "./Perguntas-Frequentes_Np_regular.png", rollover: "./Perguntas-Frequentes_NRp_regularOver.png", w: 120, h: 57 }
 };
 
 $(document).ready(function(){
 	$.fn.nofNavBarOptions({ navBarId: "NavigationBar1", rollover: true, autoClose: true });
 	$("#NavigationBar1").nofNavBar({isMain: true, orientation: "horizontal" });
-	$("#NavigationBar1_1").nofNavBar({isMain: false, orientation: "vertical", opening: "right", offsetX: 101, offsetY: 0 });
+	$("#NavigationBar1_1").nofNavBar({isMain: false, orientation: "vertical", opening: "right", offsetX: 120, offsetY: 0 });
+	$("#NavigationBar1_2").nofNavBar({isMain: false, orientation: "vertical", opening: "right", offsetX: 120, offsetY: 0 });
+	$("#NavigationBar1_3").nofNavBar({isMain: false, orientation: "vertical", opening: "right", offsetX: 120, offsetY: 0 });
+	$("#NavigationBar1_4").nofNavBar({isMain: false, orientation: "vertical", opening: "right", offsetX: 120, offsetY: 0 });
 	$("#NavigationBar1 ul").hide();
 });
 
